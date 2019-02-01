e4c787056a53a92a852c0f2df6b48efda4cabb1b
mccurrec <mccurrec@rose-hulman.edu>
2019-01-31 20:38:03 -0500

    done

--- src/problem5.py
+++ src/problem5.py
@@ -3,9 +3,9 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Ezrie McCurry.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,10 +145,19 @@ def problem5(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    largests = []
+    for j in range(len(seq_of_seq)):
+        if len(seq_of_seq[j]) > 0:
+            largest_of_subseq = seq_of_seq[j][0]
+        for k in range(1, len(seq_of_seq[j])):
+            if seq_of_seq[j][k] > largest_of_subseq:
+                largest_of_subseq = seq_of_seq[j][k]
+        if len(seq_of_seq[j]) > 0:
+            largests = largests + [largest_of_subseq]
+    return largests
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
