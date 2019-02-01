e52b031c7a77fbdf20f36af304a8f221f98260c8
mccurrec <mccurrec@rose-hulman.edu>
2019-01-31 20:18:06 -0500

    done

--- src/problem1.py
+++ src/problem1.py
@@ -3,9 +3,9 @@ Exam 3, problem 1.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Ezrie McCurry.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -126,7 +126,7 @@ def problem1(n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # **** IMPORTANT ****:
     # We supplied a   fibonacci   function above.
@@ -139,6 +139,16 @@ def problem1(n):
     # and THEN try the full-credit version.
     # As always, CONTINUE to the next problem if you are STUCK on this one.
     # -------------------------------------------------------------------------
+    fib_seq = [0, 1]
+    fib = 1
+    k = 1
+    while True:
+        fib = fib + fib_seq[k - 1]
+        fib_seq = fib_seq + [fib]
+        k = k + 1
+        if fib >= n:
+            break
+    return fib_seq[k - 1]
 
 
 ###############################################################################
